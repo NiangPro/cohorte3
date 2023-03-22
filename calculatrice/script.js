a5d9c735a00ca25f457f37f24471b541340e50b1
NiangPro <NiangProgrammeur@gmail.com>
2023-03-22 18:03:24 +0000

    calculatrice

--- calculatrice/script.js
+++ calculatrice/script.js
@@ -1,3 +1,43 @@
-let sayHello = ()=>{
-    alert("Bonjour tout le monde")
+let valeur1 = '', valeur2 = '', operateur = '', resultat = '';
+let entete = document.getElementById("entete");
+
+entete.innerText = '0';
+
+let ajouter = (valeur) =>{
+    if (operateur) {       
+        valeur2 = valeur2 == '0'? valeur: valeur2+valeur;
+        entete.innerText = valeur1+operateur+valeur2;
+    }else{
+        valeur1 =  valeur1 == '0'? valeur: valeur1+valeur;
+        entete.innerText = valeur1;
+    }
 }
+
+let  executer = (opt)=>{
+    if (valeur1) {
+        operateur = opt;
+        entete.innerText = valeur1 + operateur;
+    }
+}
+
+let effacer = ()=>{
+    if (operateur) {
+        if (valeur2) {
+            if (valeur2.length > 1) {
+                valeur2 = valeur2.substring(0, valeur2.length-1);
+                entete.innerText = valeur1+operateur+valeur2;
+
+            }else{
+                valeur2 = '';
+                entete.innerText = valeur1 + operateur;
+
+            }
+        } else {
+            operateur = '';
+            entete.innerText = valeur1;
+        }
+    } else {
+        
+    }
+}
+
